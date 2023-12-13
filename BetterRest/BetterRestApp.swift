c35ba7c65799a31abb7f00612e45756e1112e344
Wojuade Abdul Afeez <wojucrown7@gmail.com>
2023-12-13 15:34:30 +0100

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Wojuade Abdul Afeez on 13/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
